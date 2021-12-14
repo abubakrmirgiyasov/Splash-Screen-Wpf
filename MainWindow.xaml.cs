f25d240aa8f3f3dc78d1c516608b54f49496f2be
abubakrmirgiyasov <abubakrmirgiyasov@gmail.com>
2021-12-14 16:40:43 +0700

    Splash Screen

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SplashScreen
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OnLoading();
+        }
+
+        private async void OnLoading()
+        {
+            await Task.Run(() =>
+            {
+            for (int i = 0; i <= 100; i++)
+            {
+                    this.progressBar.Dispatcher.Invoke(new Action(() =>
+                    {
+                        progressBar.Value = i;
+                        textBlock.Text = $"{i} %";
+                    }));
+                    Thread.Sleep(20);
+                }
+            });
+            AnotherWindow anotherWindow = new AnotherWindow();
+            anotherWindow.Show();
+            Close();
+        }
+    }
+}
